6eb6a62113b44b19f5817d45e56b972b48a28d28
Tomas <fichtner.dk@hotmail.com>
2021-08-19 12:47:40 -0300

    Guia finalizado

--- src/main/java/com/example/spring/Customer.java
+++ src/main/java/com/example/spring/Customer.java
@@ -1,6 +1,5 @@
 package com.example.spring;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
